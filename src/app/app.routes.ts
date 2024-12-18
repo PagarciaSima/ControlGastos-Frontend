b44809d306b9960f02eb61c271bdb99a95f1991f
pagarciaSim <pablo.garciasimavilla@gmail.com>
2024-12-18 11:43:20 +0100

    Creating routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,18 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './paginas/home/home.component';
+import { LoginComponent } from './paginas/login/login.component';
+import { UsuariosComponent } from './paginas/usuarios/usuarios.component';
+import { ProveedoresComponent } from './paginas/proveedores/proveedores.component';
+import { GastosFijosComponent } from './paginas/gastos-fijos/gastos-fijos.component';
+import { GastosPorDiaComponent } from './paginas/gastos-por-dia/gastos-por-dia.component';
 
 export const routes: Routes = [
 
-    { path: "", component: HomeComponent }
+    { path: "", component: HomeComponent },
+    { path: "login", component: LoginComponent },
+    { path: "usuarios", component: UsuariosComponent },
+    { path: "proveedores", component: ProveedoresComponent },
+    { path: "gastos-fijos", component: GastosFijosComponent },
+    { path: "gastos-por-dia", component: GastosPorDiaComponent }
+
 ];
