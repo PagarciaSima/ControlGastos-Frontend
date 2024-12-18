7aa0051c2c9c7b465b47e5d608931b0548c78d57
pagarciaSim <pablo.garciasimavilla@gmail.com>
2024-12-18 10:55:23 +0100

    Digital clock

--- src/app/componentes/header/header.component.ts
+++ src/app/componentes/header/header.component.ts
@@ -1,20 +1,33 @@
-import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { DatePipe } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
 import dayjs from 'dayjs';
 import "dayjs/locale/es"
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [CommonModule],
+  imports: [DatePipe],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit{
+
+  hora!: Date;
+
+  ngOnInit(): void {
+    this.getHoraActual();
+  }
 
   getFechaActual() {
     dayjs.locale('es');
     let fecha = new Date();
     return dayjs().format('dddd, D [de] MMMM [de] YYYY');  
+  }
+
+  getHoraActual() {
+    this.hora = new Date();
+    setInterval(() => {
+      this.hora = new Date();
+    }, 1000)
   }
 }
