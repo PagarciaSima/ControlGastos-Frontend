dba19136c50214f45ab9473a7e9f7fb3bbe4672b
pagarciaSim <pablo.garciasimavilla@gmail.com>
2024-12-18 07:26:10 +0100

    header, menu, footer components

--- src/app/paginas/home/home.component.ts
+++ src/app/paginas/home/home.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { FooterComponent } from "../../componentes/footer/footer.component";
+import { HeaderComponent } from '../../componentes/header/header.component';
+import { MenuComponent } from "../../componentes/menu/menu.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [FooterComponent, HeaderComponent, MenuComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
