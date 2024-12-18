5d914c9b13ddb32266ad5ac5a1bb468afb8c3a68
pagarciaSim <pablo.garciasimavilla@gmail.com>
2024-12-18 10:44:23 +0100

    Implementing dayjs in header

--- src/app/componentes/header/header.component.ts
+++ src/app/componentes/header/header.component.ts
@@ -1,12 +1,20 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import dayjs from 'dayjs';
+import "dayjs/locale/es"
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
 
+  getFechaActual() {
+    dayjs.locale('es');
+    let fecha = new Date();
+    return dayjs().format('dddd, D [de] MMMM [de] YYYY');  
+  }
 }
