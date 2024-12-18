369fd7453fded816d7052fb8b0e9d10d09654f92
pagarciaSim <pablo.garciasimavilla@gmail.com>
2024-12-18 12:13:31 +0100

    404 page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,7 @@ import { UsuariosComponent } from './paginas/usuarios/usuarios.component';
 import { ProveedoresComponent } from './paginas/proveedores/proveedores.component';
 import { GastosFijosComponent } from './paginas/gastos-fijos/gastos-fijos.component';
 import { GastosPorDiaComponent } from './paginas/gastos-por-dia/gastos-por-dia.component';
+import { Error404Component } from './paginas/error404/error404.component';
 
 export const routes: Routes = [
 
@@ -13,6 +14,7 @@ export const routes: Routes = [
     { path: "usuarios", component: UsuariosComponent },
     { path: "proveedores", component: ProveedoresComponent },
     { path: "gastos-fijos", component: GastosFijosComponent },
-    { path: "gastos-por-dia", component: GastosPorDiaComponent }
+    { path: "gastos-por-dia", component: GastosPorDiaComponent },
+    { path: "**", component: Error404Component }
 
 ];
