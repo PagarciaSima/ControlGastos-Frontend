63f17a03deb70b6c0abbeb5fade906ac6ae4823a
pagarciaSim <pablo.garciasimavilla@gmail.com>
2024-12-20 15:19:58 +0100

    loading current month

--- src/app/paginas/gastos-fijos/gastos-fijos.component.ts
+++ src/app/paginas/gastos-fijos/gastos-fijos.component.ts
@@ -4,21 +4,34 @@ import { FooterComponent } from '../../componentes/footer/footer.component';
 import { HeaderComponent } from '../../componentes/header/header.component';
 import { FormsModule } from '@angular/forms';
 import { RouterLink } from '@angular/router';
+import dayjs from 'dayjs';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-gastos-fijos',
   standalone: true,
-  imports: [MenuComponent, FooterComponent, HeaderComponent, FormsModule, RouterLink],
+  imports: [MenuComponent, FooterComponent, HeaderComponent, FormsModule, RouterLink, CommonModule],
   templateUrl: './gastos-fijos.component.html',
   styleUrl: './gastos-fijos.component.css'
 })
 export class GastosFijosComponent implements OnInit{
   
+  fecha!: Date;
+
+  constructor() {
+    this.fecha = new Date();
+  }
+
   ngOnInit(): void {
-    throw new Error('Method not implemented.');
   }
 
   modalCrear() {
 
+  }
+
+  getMesActual() {
+    let date = new Date();
+    dayjs.locale('es');
+    return dayjs(date).format('MMMM');
   }
 }
